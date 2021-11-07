b6e7eb5ba1d5986b623849b53a403120828fb68c
Anny <anny.bevilacqua@studio.unibo.it>
2021-11-07 01:15:39 +0100

    Update exceptions handling in BaseRobotTest

--- src/it/unibo/oop/lab/exception1/BaseRobotTest.java
+++ src/it/unibo/oop/lab/exception1/BaseRobotTest.java
@@ -36,15 +36,15 @@ public final class BaseRobotTest {
         /*
          * 2) Move the robot right until it touches the world limit
          */
-        for (int i = 0; i < RobotEnvironment.WORLD_X_UPPER_LIMIT; i++) {
-            // check if position if coherent
-        		assertTrue("[CHECKING MOVING RIGHT]", r1.moveRight());
-        }
-        // reached the right limit of the world
         try {
+	        for (int i = 0; i < RobotEnvironment.WORLD_X_UPPER_LIMIT; i++) {
+	            // check if position if coherent
+	        		assertTrue("[CHECKING MOVING RIGHT]", r1.moveRight());
+	        }
+	        // reached the right limit of the world
         	assertFalse("[CHECKING MOVING RIGHT]", r1.moveRight());
         	// fail();
-        } catch (PositionOutOfBoundException e) {
+        } catch (Exception e) {
         	assertNotNull(e.getMessage());
         	System.out.println("Exception caught! " + e.getMessage() + ".");
         }
@@ -55,15 +55,15 @@ public final class BaseRobotTest {
         /*
          * 2) Move to the top until it reaches the upper right conrner of the world
          */
-        for (int i = 0; i < RobotEnvironment.WORLD_Y_UPPER_LIMIT; i++) {
-            // check if position if coherent
-            assertTrue("[CHECKING MOVING UP]", r1.moveUp());
-        }
-        // reached the upper limit of the world
         try {
+	        for (int i = 0; i < RobotEnvironment.WORLD_Y_UPPER_LIMIT; i++) {
+	            // check if position if coherent
+	            assertTrue("[CHECKING MOVING UP]", r1.moveUp());
+	        }
+	        // reached the upper limit of the world
         	assertFalse("[CHECKING MOVING UP]", r1.moveUp());
         	//fail();
-        } catch (PositionOutOfBoundException e) {
+        } catch (Exception e) {
         	assertNotNull(e.getMessage());
         	System.out.println("Exception caught! " + e.getMessage() + ".");        	
         }
@@ -83,18 +83,18 @@ public final class BaseRobotTest {
          * Repeatedly move the robot up and down until the battery is completely
          * exhausted.
          */
-        while (r2.getBatteryLevel() > 0) {
-            r2.moveUp();
-            r2.moveDown();
-        }
-        // verify battery level:
-        // expected, actual, delta (accepted error as we deal with decimal
-        // values: in this case we accept NO ERROR, which is generally bad)
-        assertEquals(0d, r2.getBatteryLevel(), 0);
-        // verify position: same as start position
-        assertEquals("[CHECKING ROBOT INIT POS Y]", 0, r2.getEnvironment().getCurrPosY());
-        // out of world: returns false
         try {
+	        while (r2.getBatteryLevel() > 0) {
+	            r2.moveUp();
+	            r2.moveDown();
+	        }
+	        // verify battery level:
+	        // expected, actual, delta (accepted error as we deal with decimal
+	        // values: in this case we accept NO ERROR, which is generally bad)
+	        assertEquals(0d, r2.getBatteryLevel(), 0);
+	        // verify position: same as start position
+	        assertEquals("[CHECKING ROBOT INIT POS Y]", 0, r2.getEnvironment().getCurrPosY());
+	        // out of world: returns false
         	assertFalse("[CHECKING MOVING UP]", r2.moveUp());
         } catch (NotEnoughBatteryException e) {
         	System.out.println("Exception caught! " + e.getMessage() + ".");
